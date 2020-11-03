9af1a051bc40009f288e421e539fd8016fdb8bf3
Qing Ma <gsmofgsm@hotmail.com>
2020-11-03 23:08:32 +0100

    add routes and page detail and cart

---
+++ src/Detail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Products from "./Products";
+
+export default function Detail() {
+  return <h1>Detail</h1>;
+
+  //   return (
+  //     <div id="detail">
+  //       <h1>{products.name}</h1>
+  //       <p>{products.description}</p>
+  //       <p id="price">${products.price}</p>
+  //       <img src={`/images/${product.image}`} alt={product.category} />
+  //     </div>
+  //   );
+}
