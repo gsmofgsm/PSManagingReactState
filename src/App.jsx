d531bcedf9d610bd816d1d1d4e3e5394e1097316
Qing Ma <gsmofgsm@hotmail.com>
2020-11-04 14:33:44 +0100

    remove an item from cart

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,12 @@ export default function App() {
 
   function updateQuantity(sku, quantity) {
     setCart((items) => {
-      return items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+      return quantity === 0
+        ? items.filter((i) => i.sku !== sku)
+        : items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+      // return items
+      //   .map((i) => (i.sku === sku ? { ...i, quantity } : i))
+      //   .filter((i) => i.quantity > 0);
     });
   }
 
