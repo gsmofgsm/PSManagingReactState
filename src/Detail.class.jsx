0f5a0927b1ef816e75adeef1b2f9311445d6b7f1
Qing Ma <gsmofgsm@hotmail.com>
2020-11-05 23:28:45 +0100

    consuming hooks example 2: Custom wrapper

--- src/Detail.class.jsx
+++ src/Detail.class.jsx
@@ -1,48 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import { useCart } from "./cartContext";
 
-export default function Detail() {
+export default function DetailWrapper() {
   const { dispatch } = useCart();
   const { id } = useParams();
-  const navigate = useNavigate();
-  const [sku, setSku] = useState("");
-  const { data: product, error, loading } = useFetch(`products/${id}`);
-
-  if (loading) return <Spinner />;
-  if (!product) return <PageNotFound />;
-  if (error) throw error;
-
+  const fetchResponse = useFetch(`products/${id}`);
   return (
-    <div id="detail">
-      <h1>what {product.name}</h1>
-      <p>{product.description}</p>
-      <p id="price">${product.price}</p>
-
-      <select id="sku" value={sku} onChange={(e) => setSku(e.target.value)}>
-        <option value="">What size</option>
-        {product.skus.map(({ sku, size }) => (
-          <option key={sku} value={sku}>
-            {size}
-          </option>
-        ))}
-      </select>
-      <p>
-        <button
-          disabled={!sku}
-          className="btn btn-primary"
-          onClick={() => {
-            dispatch({ type: "add", id, sku });
-            navigate("/cart");
-          }}
-        >
-          Add to cart
-        </button>
-      </p>
-      <img src={`/images/${product.image}`} alt={product.category} />
-    </div>
+    <Detail
+      id={id}
+      fetchResponse={fetchResponse}
+      navigate={useNavigate()}
+      dispatch={dispatch}
+    ></Detail>
   );
+}
+
+export class Detail extends React.Component {
+  state = {
+    sku: "",
+  };
+
+  render() {
+    const { id, fetchResponse, navigate, dispatch } = this.props;
+    const { data: product, loading, error } = fetchResponse;
+    const { sku } = this.state;
+
+    if (loading) return <Spinner />;
+    if (!product) return <PageNotFound />;
+    if (error) throw error;
+
+    return (
+      <div id="detail">
+        <h1>what {product.name}</h1>
+        <p>{product.description}</p>
+        <p id="price">${product.price}</p>
+
+        <select
+          id="sku"
+          value={sku}
+          onChange={(e) => this.setState({ sku: e.target.value })}
+        >
+          <option value="">What size</option>
+          {product.skus.map(({ sku, size }) => (
+            <option key={sku} value={sku}>
+              {size}
+            </option>
+          ))}
+        </select>
+        <p>
+          <button
+            disabled={!sku}
+            className="btn btn-primary"
+            onClick={() => {
+              dispatch({ type: "add", id, sku });
+              navigate("/cart");
+            }}
+          >
+            Add to cart
+          </button>
+        </p>
+        <img src={`/images/${product.image}`} alt={product.category} />
+      </div>
+    );
+  }
 }
