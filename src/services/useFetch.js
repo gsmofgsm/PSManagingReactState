ac3d92b6f5a925660564ac5d9bd254beebabf932
Qing Ma <gsmofgsm@hotmail.com>
2020-11-05 09:18:44 +0100

    do not setState on unmounted component

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMounted = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMounted.current = true;
     const init = async () => {
       try {
         const reponse = await fetch(baseUrl + url);
         if (reponse.ok) {
           const json = await reponse.json();
-          setData(json);
+          if (isMounted.currrent) setData(json);
         } else {
           throw reponse;
         }
       } catch (e) {
-        setError(e);
+        if (isMounted.currrent) setError(e);
       } finally {
-        setLoading(false);
+        if (isMounted.currrent) setLoading(false);
       }
     };
     init();
+
+    return () => {
+      // function returned from useEffect will be executed when unmounted
+      isMounted.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
