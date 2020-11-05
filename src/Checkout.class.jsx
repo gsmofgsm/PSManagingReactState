b376135191836f426a894be389bf92431088564b
Qing Ma <gsmofgsm@hotmail.com>
2020-11-05 20:11:22 +0100

    class component, state, rendor method

--- src/Checkout.class.jsx
+++ src/Checkout.class.jsx
@@ -15,16 +15,29 @@ const emptyAddress = {
 };
 
 export default class Checkout extends React.Component {
-  const [address, setAddress] = useState(emptyAddress);
-  const [status, setStatus] = useState(STATUS.IDLE);
-  const [saveError, setSaveError] = useState(null);
-  const [touched, setTouched] = useState({});
+  state = {
+    address: emptyAddress,
+    status: STATUS.IDLE,
+    saveErrorr: null,
+    touched: {},
+  }; // use class field instead of constructor requires less code
+  // constructor(props) {
+  //   super(props); // this must be the first line so that base class receives props
 
-  // Derived state
-  const errors = getErrors(address);
-  const isValid = Object.keys(errors).length === 0;
+  //   this.state = {
+  //     address: emptyAddress,
+  //     status: STATUS.IDLE,
+  //     saveErrorr: null,
+  //     touched: {}
+  //   }
+  // }
 
-  function handleChange(e) {
+  isValid() {
+    const errors = getErrors(this.state.address);
+    return Object.keys(errors).length === 0;
+  }
+
+  handleChange(e) {
     e.persist(); // persist the event,
     // otherwise react will gabage collect it before the function call,
     // only neccessary for react < 17 and when use setState function
@@ -36,14 +49,14 @@ export default class Checkout extends React.Component {
     });
   }
 
-  function handleBlur(event) {
+  handleBlur(event) {
     event.persist(); // persist the event,
     setTouched((cur) => {
       return { ...cur, [event.target.id]: true };
     });
   }
 
-  async function handleSubmit(event) {
+  async handleSubmit(event) {
     event.preventDefault();
     setStatus(STATUS.SUBMITTING);
     if (isValid) {
@@ -59,76 +72,80 @@ export default class Checkout extends React.Component {
     }
   }
 
-  function getErrors(address) {
+  getErrors(address) {
     const result = {};
     if (!address.city) result.city = "City is required";
     if (!address.country) result.country = "Country is required";
     return result;
   }
 
-  if (saveError) throw saveError;
-  if (status === STATUS.COMPLETED) {
-    return <h1>Thanks for shopping!</h1>;
+  rendor() {
+    // jsx by rendor method
+    if (saveError) throw saveError;
+    if (status === STATUS.COMPLETED) {
+      return <h1>Thanks for shopping!</h1>;
+    }
+
+    return (
+      <>
+        <h1>Shipping Info</h1>
+        {!isValid && status === STATUS.SUBMITTED && (
+          <div role="alert">
+            <p>Please fix the following errors:</p>
+            <ul>
+              {Object.keys(errors).map((key) => {
+                return <li key={key}>{errors[key]}</li>;
+              })}
+            </ul>
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="city">City</label>
+            <br />
+            <input
+              id="city"
+              type="text"
+              value={address.city}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            <p role="alert">
+              {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+            </p>
+          </div>
+
+          <div>
+            <label htmlFor="country">Country</label>
+            <br />
+            <select
+              id="country"
+              value={address.country}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            >
+              <option value="">Select Country</option>
+              <option value="China">China</option>
+              <option value="India">India</option>
+              <option value="United Kingdom">United Kingdom</option>
+              <option value="USA">USA</option>
+            </select>
+            <p role="alert">
+              {(touched.country || status === STATUS.SUBMITTED) &&
+                errors.country}
+            </p>
+          </div>
+
+          <div>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Save Shipping Info"
+              disabled={status === STATUS.SUBMITTING}
+            />
+          </div>
+        </form>
+      </>
+    );
   }
-
-  return (
-    <>
-      <h1>Shipping Info</h1>
-      {!isValid && status === STATUS.SUBMITTED && (
-        <div role="alert">
-          <p>Please fix the following errors:</p>
-          <ul>
-            {Object.keys(errors).map((key) => {
-              return <li key={key}>{errors[key]}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="city">City</label>
-          <br />
-          <input
-            id="city"
-            type="text"
-            value={address.city}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          />
-          <p role="alert">
-            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
-          </p>
-        </div>
-
-        <div>
-          <label htmlFor="country">Country</label>
-          <br />
-          <select
-            id="country"
-            value={address.country}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          >
-            <option value="">Select Country</option>
-            <option value="China">China</option>
-            <option value="India">India</option>
-            <option value="United Kingdom">United Kingdom</option>
-            <option value="USA">USA</option>
-          </select>
-          <p role="alert">
-            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
-          </p>
-        </div>
-
-        <div>
-          <input
-            type="submit"
-            className="btn btn-primary"
-            value="Save Shipping Info"
-            disabled={status === STATUS.SUBMITTING}
-          />
-        </div>
-      </form>
-    </>
-  );
 }
