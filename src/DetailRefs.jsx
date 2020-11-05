c0b423c2b857043c2050bbb1d1a234767fec1a57
Qing Ma <gsmofgsm@hotmail.com>
2020-11-05 09:10:03 +0100

    useRefs as alternative to useState

---
+++ src/DetailRefs.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+
+export default function Detail(props) {
+  const { id } = useParams();
+  const skuRef = useRef();
+  const navigate = useNavigate();
+  const { data: product, error, loading } = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>what {product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+
+      <select id="sku" ref={skuRef}>
+        <option value="">What size</option>
+        {product.skus.map(({ sku, size }) => (
+          <option key={sku} value={sku}>
+            {size}
+          </option>
+        ))}
+      </select>
+      <p>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            const sku = skuRef.current.value;
+            if (!sku) return alert("Select size.");
+            props.addToCart(id, sku);
+            navigate("/cart");
+          }}
+        >
+          Add to cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
+}
